e4a2941631cefb235002a0659f230368521ecb9d
juliana <juliana.silva@efdv.g12.br>
2024-10-16 15:48:32 -0300

    condicional

---
+++ script.js
@@ -0,0 +1,30 @@
+//condicionias é uma forma de tomar decisões, de fazer o computador executar essas decisõe.
+
+//variaveis
+//3 var, let e const 
+
+//prompt pergunta algo ao usuário 
+//sempre que usamos dados textuais
+
+    //let nome = prompt("Qual é seu nome?");
+
+//alert usamos para exibir uma mensagem 
+  //alert(Seja bem vindo(a), + nome);
+
+//let idade = prompt (" Quantos anos voce tem?")
+//if significa "se" 
+//if (condição) {código que será executado}
+// else significa "senão"
+
+//if (idade >= 18) { 
+    //alert ("você já pode tirar a carteira de motorista !!!")
+//}else{
+    // alert ("Você ainda não tem idade para dirigir!!!")}
+
+    let gostarDePizza = prompt ("Você gosta de pizza? responda (sim ou não)")
+    //=== estritamente igual 
+if (gostarDePizza === "sim"){ 
+    alert (" Que ótimo! Vamos comer juntos!")
+} else {
+    alert("Tudo bem, talvez você goste de outra comida.")
+}
